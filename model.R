d9a3714da93ba5870527251ff8ac56e246f9595c
jacekchess <j.wisniewski@vp.pl>
2021-04-15 12:09:01 +0200

    Podstawowe tunningowanie parametrow

--- model.R
+++ model.R
@@ -2,6 +2,8 @@ library(mlr)
 library(dplyr)
 library(pROC)
 
+### Preparing data
+
 phones <- read.csv('./phones.csv')
 phones[is.na(phones)] <- 0 # NA means phone does not have camera
 phones <- phones[, -c(1, 9, 10)] # removing name, height, width beacause they are useless
@@ -11,18 +13,81 @@ task <- makeRegrTask(id = 'phones', data = phones, target = "price")
 
 # lrns <- as.data.frame(listLearners())
 # lrns <- lrns[lrns$type == 'regr', ]
+
+#################################################################################
+#################################################################################
+
+### Testing hyper parameteres
+
 # getLearnerParamSet("regr.svm")
 
-learner <- makeLearner("regr.svm")
+kernel <- c('linear', 'polynomial', 'radial', 'sigmoid')
+
+for (ker in kernel) {
+  learner <- makeLearner("regr.svm", par.vals = list(kernel = ker))
+  cv <- makeResampleDesc("CV", iters = 5)
+  resampled <- resample(learner, task, cv, measures = list(rmse, mae))
+  print(ker)
+  print(resampled$aggr)
+}
+# radial is the best kernel
+
+###########################
+
+types <- c('eps-regression', 'nu-regression')
+
+eps <- c()
+nu <- c()
+for (i in 1:50) {
+  learner <- makeLearner("regr.svm", par.vals = list(type = 'eps-regression'))
+  cv <- makeResampleDesc("CV", iters = 5)
+  resampled <- resample(learner, task, cv, measures = list(rmse, mae))
+  eps[i] <- resampled$aggr
+  
+  learner <- makeLearner("regr.svm", par.vals = list(type = 'nu-regression'))
+  cv <- makeResampleDesc("CV", iters = 5)
+  resampled <- resample(learner, task, cv, measures = list(rmse, mae))
+  nu[i] <- resampled$aggr
+}
+summary(eps)
+summary(nu)
+# eps-regression is the best type
+
+#################################
+
+degrees <- data.frame(matrix(, nrow = 10, ncol = 10))
+for (i in 1:10) {
+  record <- c()
+  for (j in 1:10) {
+    learner <- makeLearner("regr.svm", par.vals = list(degree = i))
+    cv <- makeResampleDesc("CV", iters = 5)
+    resampled <- resample(learner, task, cv, measures = list(rmse, mae))
+    record[j] <- resampled$aggr
+  }
+  degrees[i, ] <- record
+}
+summary(degrees)
+# degree = 3 is the best from 1 - 10 range
+
+#################################################################################
+#################################################################################
+
+# Creating model
+
+learner <- makeLearner("regr.svm", par.vals = list(type = "eps-regression",
+                                                   kernel = "radial",
+                                                   degree = 3))
+# getHyperPars(learner)
 
 cv <- makeResampleDesc("CV", iters = 5)
 # listMeasures(obj = "regr")
 resampled <- resample(learner, task, cv, measures = list(rmse, mae))
-# resampled$aggr
+resampled$aggr
 # resampled$pred$data
 
-plot(1:414, resampled$pred$data$truth)
-plot(1:414, resampled$pred$data$response)
 
-roc_curve <- roc(resampled$pred$data$truth, resampled$pred$data$response)
-plot(roc_curve)
+# plot(1:414, resampled$pred$data$truth)
+# plot(1:414, resampled$pred$data$response)
+
+# roc_curve <- roc(resampled$pred$data$truth, resampled$pred$data$response)
+# plot(roc_curve)
